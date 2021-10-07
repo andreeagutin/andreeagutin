689ddac7bc6633795212c7d2eacf59bae8cf757d
andreeagutin <andreea.gutin@gmail.com>
2021-10-08 00:33:11 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+    return (
+        <header>
+            <h1>{props.name}</h1>
+        </header>
+    );
+}
+function Main(props) {
+    return (
+        <section>
+            <h2>Experience: Andreea's {props.adjective}</h2>
+            <img
+                src="https://github.com/andreeagutin.png"
+                height="200"
+                alt="Andreea Gutin's website"
+            />
+            <ul style={{ textAlign: "left" }}>
+                {props.companies.map((company, i) => (
+                    <li key={company.id}>{company.title}</li>
+                ))}
+            </ul>
+        </section>
+    );
+}
+function Footer(props) {
+    return (
+        <footer>
+            <h3>Copyright {props.year}</h3>
+        </footer>
+    );
+}
+
+const companies = [
+    "Medidata Solutions",
+    "Tagident",
+    "BosmanGgz",
+    "AlienConcept"
+];
+
+const companiesObjects = companies.map((company, i) => ({ id: i, title: company }));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header name="Andreea Gutin" />
+            <Main adjective="blog" companies={companiesObjects} />
+            <Footer year={new Date().getFullYear()} />
+        </div>
+    );
 }
 
 export default App;
